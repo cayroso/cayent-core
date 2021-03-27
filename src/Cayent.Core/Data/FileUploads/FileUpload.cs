3b6acedd852d2e9e8bc32d83a6c76d5c461ef8ed
Chris Ayroso <caydev2010@gmail.com>
2021-03-27 17:10:32 +0800

    123

---
+++ src/Cayent.Core/Data/FileUploads/FileUpload.cs
@@ -0,0 +1,25 @@
+﻿using Cayent.Core.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cayent.Core.Data.Fileuploads
+{
+    public class FileUpload
+    {
+        public string FileUploadId { get; set; }
+        public string Url { get; set; }
+        public string FileName { get; set; }
+        public string ContentDisposition { get; set; }
+        public string ContentType { get; set; }
+        public byte[] Content { get; set; }
+        public long Length { get; set; }
+
+        DateTime _dateCreated = DateTime.UtcNow;
+        public DateTime DateCreated
+        {
+            get => _dateCreated.AsUtc();
+            set => _dateCreated = value.Truncate();
+        }
+    }
+}
