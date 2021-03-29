ee104a64ed1c1acf80c9471cc95dc4f32c9e1d45
Christopher Ayroso <ChristopherAyroso@deltek.com>
2021-03-30 07:26:01 +0800

    xxxxxxx

--- src/Cayent.Core/Common/Extensions/IQueryableExtensions.cs
+++ src/Cayent.Core/Common/Extensions/IQueryableExtensions.cs
@@ -3,20 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cayent.Core.Common.Extensions
 {
     public static class IQueryableExtensions
     {
-        public static async Task<Paged<T>> ToPagedItemsAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize) where T : class
+        public static async Task<Paged<T>> ToPagedItemsAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize, CancellationToken cancellationToken = default) where T : class
         {
             var count = await source.CountAsync().ConfigureAwait(false);
             var collection = await source
                 .AsNoTracking()
                 .Skip((pageIndex - 1) * pageSize)
                 .Take(pageSize)
-                .ToListAsync()
+                .ToListAsync(cancellationToken)
                 .ConfigureAwait(false);
 
             return new Paged<T>(collection, count, pageIndex, pageSize);
---
+++ src/Cayent.Core/Data/Calendars/Calendar.cs
@@ -0,0 +1,34 @@
+﻿using Cayent.Core.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cayent.Core.Data.Calendars
+{
+    public class Calendar
+    {
+        public Calendar()
+        {
+            //JobStatusHistory = new List<JobStatusHistory>();
+        }
+
+        DateTime _date;
+        public DateTime Date
+        {
+            get => _date.AsUtc();
+            set => _date = value.Truncate();
+        }
+
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int Day { get; set; }
+        public int Quarter { get; set; }
+
+        public string MonthName { get; set; }
+        public int DayOfYear { get; set; }
+        public int DayOfWeek { get; set; }
+        public string DayName { get; set; }
+
+        //public virtual ICollection<JobStatusHistory> JobStatusHistory { get; set; }
+    }
+}
