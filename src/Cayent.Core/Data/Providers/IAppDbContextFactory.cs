615e6bdbe649dd3a79ccb46c158410221af3deb8
Chris Ayroso <caydev2010@gmail.com>
2021-04-05 19:56:14 +0800

    kkkkkkkkkk

---
+++ src/Cayent.Core/Data/Providers/IAppDbContextFactory.cs
@@ -0,0 +1,155 @@
+﻿using Cayent.Core.Data.Identity.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cayent.Core.Data.Providers
+{
+    //public interface IAppDbContextFactory
+    //{
+    //    void Provision(Tenant tenant, List<ProvisionUserRole> users, bool isDevelopment);
+
+    //    AppDbContext Get();
+
+    //    string GenerateConnectionString(string serverName, string databaseName);
+    //}
+
+    //public class DefaultAppDbContextFactory : IAppDbContextFactory//,  IDesignTimeDbContextFactory<AppDbContext>	
+    //{
+    //    readonly DbContextOptions<AppDbContext> _options;
+    //    readonly IConfiguration _configuration;
+    //    readonly ITenantProvider _tenantProvider;
+
+    //    public DefaultAppDbContextFactory(DbContextOptions<AppDbContext> options, IConfiguration configuration, ITenantProvider tenantProvider)
+    //    {
+    //        _options = options ?? throw new ArgumentNullException(nameof(options));
+    //        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+    //        _tenantProvider = tenantProvider ?? throw new ArgumentNullException(nameof(tenantProvider));
+    //    }
+
+    //    public void Provision(Tenant tenant, List<ProvisionUserRole> provUserRoles, bool isDevelopment)
+    //    {
+    //        throw new NotImplementedException("Provision should not be called.");
+    //        //var ctx = new AppDbContext(_options, _configuration, new DummyTenantProvider(tenant));
+
+    //        //if (!ctx.Database.CanConnect())
+    //        //    ctx.Database.Migrate();
+
+    //        //var roles = new List<Role>();
+
+    //        ////  create roles
+    //        //ApplicationRoles.Items.ForEach(r =>
+    //        //{
+    //        //    var role = new Role
+    //        //    {
+    //        //        RoleId = r.Id,
+    //        //        Name = r.Name
+    //        //    };
+
+    //        //    roles.Add(role);
+    //        //});
+
+    //        //ctx.AddRange(roles);
+
+
+    //        ////  add user then assign roles
+    //        //provUserRoles.ForEach(provUserRole =>
+    //        //{
+    //        //    var userRoles = new List<UserRole>();
+
+    //        //    provUserRole.RoleIds.ForEach(roleId =>
+    //        //    {
+    //        //        var userRole = new UserRole
+    //        //        {
+    //        //            UserId = provUserRole.User.UserId,
+    //        //            RoleId = roleId
+    //        //        };
+
+    //        //        userRoles.Add(userRole);
+    //        //    });
+
+    //        //    ctx.Add(provUserRole.User);
+    //        //    ctx.AddRange(userRoles);
+    //        //});
+
+    //        ////  add default data
+    //        //if (!ctx.Calendars.Any() && isDevelopment)
+    //        //{
+    //        //    GenerateCalendar(ctx, tenant);
+    //        //    AppDbContextInitializer.Initialize(ctx, provUserRoles);
+    //        //}
+
+    //        //ctx.SaveChanges();
+    //    }
+
+    //    public AppDbContext Get()
+    //    {
+    //        var ctx = new AppDbContext(_options, _configuration, _tenantProvider);
+
+    //        return ctx;
+    //    }
+
+    //    public string GenerateConnectionString(string serverName, string databaseName)
+    //    {
+    //        var ctx = new AppDbContext(_options, _configuration, _tenantProvider);
+    //        var connString = string.Empty;
+
+    //        if (ctx.Database.IsSqlServer())
+    //        {
+    //            connString = $@"Server={serverName};Database={databaseName};trusted_connection=true;";
+    //        }
+    //        else if (ctx.Database.IsSqlite())
+    //        {
+    //            connString = $"Data Source=App_Data\\{databaseName}.db;";
+    //        }
+    //        else
+    //        {
+    //            throw new ApplicationException($"Not supported database provider; {ctx.Database.ProviderName}");
+    //        }
+
+    //        return connString;
+    //    }
+
+    //    void GenerateCalendar(AppDbContext ctx, Tenant tenant)
+    //    {
+    //        var utcnow = DateTime.UtcNow;
+
+    //        var start = utcnow.AddDays(-utcnow.DayOfYear);// new DateTime(2020, 1, 1);
+    //        var end = start.AddYears(1);// new DateTime(2022, 12, 31);
+
+    //        var now = start;
+
+    //        var cals = new List<Calendar>();
+
+    //        while (now <= end)
+    //        {
+    //            var cal = new Calendar
+    //            {
+    //                Date = now,
+    //                Year = now.Year,
+    //                Month = now.Month,
+    //                Day = now.Day,
+    //                Quarter = (now.Month + 2) / 3,
+    //                MonthName = now.ToString("MMMMM"),
+    //                DayOfYear = now.DayOfYear,
+    //                DayOfWeek = (int)now.DayOfWeek,
+    //                DayName = now.ToString("dddd")
+    //            };
+
+    //            cals.Add(cal);
+
+    //            now = now.AddDays(1);
+    //        }
+
+    //        ctx.AddRange(cals);
+    //    }
+
+    //}
+
+    //public class ProvisionUserRole
+    //{
+    //    public User User { get; set; }
+    //    public List<string> RoleIds { get; set; } = new List<string>();
+
+    //}
+}
---
+++ src/Cayent.Core/Data/Providers/ITenantProvider.cs
@@ -0,0 +1,58 @@
+﻿using Cayent.Core.Data.Identity.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cayent.Core.Data.Providers
+{
+    //public interface ITenantProvider
+    //{
+    //    Tenant GetTenant();
+    //}
+    //public class DummyTenantProvider : ITenantProvider
+    //{
+    //    Tenant _tenant;
+
+    //    public DummyTenantProvider(Tenant tenant)
+    //    {
+    //        _tenant = tenant;
+    //    }
+    //    public Tenant GetTenant()
+    //    {
+    //        return _tenant;
+    //    }
+
+    //}
+
+    //public class DefaultTenantProvider : ITenantProvider
+    //{
+    //    string _tenantId;
+    //    IdentityWebContext _webContext;
+
+    //    public DefaultTenantProvider(IHttpContextAccessor accessor, IdentityWebContext webContext)
+    //    {
+    //        if (accessor != null && accessor.HttpContext != null)
+    //        {
+    //            _tenantId = accessor.HttpContext.User.Claims.FirstOrDefault(p => p.Type == "TenantId")?.Value;
+    //        }
+
+    //        _webContext = webContext;
+    //    }
+
+    //    public Tenant GetTenant()
+    //    {
+    //        if (!string.IsNullOrWhiteSpace(_tenantId))
+    //        {
+    //            var tenant = _webContext
+    //                .Tenants
+    //                .Include(p => p.Users)
+    //                .AsNoTracking()
+    //                .FirstOrDefault(p => p.TenantId == _tenantId);
+
+    //            return tenant;
+    //        }
+
+    //        return null;
+    //    }
+    //}
+}
