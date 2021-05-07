47481d3faabfb4599d8cee5fbbc5731d1b97f0b1
ChristopherAyroso <caydev2010@gmail.com>
2021-05-07 21:28:01 +0800

    123

--- src/Cayent.Core/CQRS/Services/IAppDbContext.cs
+++ src/Cayent.Core/CQRS/Services/IAppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Cayent.Core.Data.Chats;
+﻿using Cayent.Core.Data.Calendars;
+using Cayent.Core.Data.Chats;
 using Cayent.Core.Data.Fileuploads;
 using Cayent.Core.Data.Notifications;
 using Cayent.Core.Data.Users;
@@ -11,10 +12,16 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+
 namespace Cayent.Core.CQRS.Services
 {
     public abstract class AppBaseDbContext: DbContext
     {
+        protected const int KeyMaxLength = 36;
+        protected const int NameMaxLength = 256;
+        protected const int DescMaxLength = 2048;
+        protected const int NoteMaxLength = 4096;
+
         public AppBaseDbContext(DbContextOptions<AppBaseDbContext> options)
             : base(options)
         {
@@ -34,12 +41,46 @@ namespace Cayent.Core.CQRS.Services
         public DbSet<ChatReceiver> ChatReceivers { get; set; }
         public DbSet<ChatMessage> ChatMessages { get; set; }
         public DbSet<FileUpload> FileUploads { get; set; }
+        public DbSet<Calendar> Calendars { get; set; }
         public DbSet<Notification> Notifications { get; set; }
         public DbSet<NotificationReceiver> NotificationReceivers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            CreateCalendar(modelBuilder);
+
+            CreateFileUploads(modelBuilder);
+
+        }
+
+        static void CreateCalendar(ModelBuilder builder)
+        {
+            builder.Entity<Calendar>(b =>
+            {                
+                b.ToTable("Calendar");
+                b.HasKey(e => e.Date);
+
+                b.Property(e => e.MonthName).HasMaxLength(KeyMaxLength).IsRequired();
+                b.Property(e => e.DayName).HasMaxLength(KeyMaxLength).IsRequired();
+            });
+
+        }
+
+        static void CreateFileUploads(ModelBuilder builder)
+        {
+            builder.Entity<FileUpload>(b =>
+            {
+                b.ToTable("FileUpload");
+                b.HasKey(e => e.FileUploadId);
+
+                b.Property(e => e.FileUploadId).HasMaxLength(KeyMaxLength).IsRequired();
+                b.Property(e => e.Url).HasMaxLength(DescMaxLength);
+                b.Property(e => e.FileName).HasMaxLength(DescMaxLength);
+                b.Property(e => e.ContentDisposition).HasMaxLength(DescMaxLength);
+                b.Property(e => e.ContentType).HasMaxLength(DescMaxLength);
+            });
         }
     }
 }
