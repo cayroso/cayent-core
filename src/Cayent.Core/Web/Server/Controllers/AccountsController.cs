9cade804551a69f8b53b2794e4f87003d6717885
ChristopherAyroso <caydev2010@gmail.com>
2021-12-17 10:52:52 +0800

    222222

---
+++ src/Cayent.Core/Web/Server/Controllers/AccountsController.cs
@@ -0,0 +1,228 @@
+﻿//using App.CQRS;
+//using App.CQRS.Navbar.Common.Queries.Query;
+//using Cayent.Core.CQRS.Queries;
+//using Data.App.DbContext;
+//using Data.App.Models.FileUploads;
+//using Data.Common;
+//using Data.Identity.DbContext;
+//using Data.Providers;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Web.Services;
+
+//namespace Web.Controllers
+//{
+//    [Authorize]
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    [Produces("application/json")]
+//    public class AccountsController : BaseController
+//    {
+//        readonly IQueryHandlerDispatcher _queryHandlerDispatcher;
+
+//        public AccountsController(IQueryHandlerDispatcher queryHandlerDispatcher)
+//        {
+//            _queryHandlerDispatcher = queryHandlerDispatcher ?? throw new ArgumentNullException(nameof(queryHandlerDispatcher));
+//        }
+
+//        [HttpPut("change-theme")]
+//        public async Task<IActionResult> ChangeThemeAsync([FromServices] IdentityWebContext webContext, string theme)
+//        {
+//            var userInfo = await webContext.UserInformations.FirstOrDefaultAsync(e => e.UserId == UserId);
+
+//            if (userInfo != null)
+//            {
+//                userInfo.Theme = theme;
+
+//                await webContext.SaveChangesAsync();
+//            }
+
+//            return Ok();
+//        }
+
+//        [HttpGet("unread-chats")]
+//        public async Task<IActionResult> GetNavbarUnreadChats(System.Threading.CancellationToken cancellationToken = default)
+//        {
+//            var query = new GetUnreadChatsQuery("", TenantId, UserId, 1, 10);
+//            var dto = await _queryHandlerDispatcher.HandleAsync<GetUnreadChatsQuery, Paged<GetUnreadChatsQuery.ChatMessage>>(query, cancellationToken);
+
+//            return Ok(dto.Items);
+//        }
+
+//        [HttpPut]
+//        public async Task<IActionResult> Put(
+//            [FromServices] IdentityWebContext webDbContext,
+//            [FromServices] AppDbContext appDbContext,
+//            [FromBody] UpdateUserInfo info
+//            )
+//        {
+//            var user = await webDbContext.UserInformations.FirstOrDefaultAsync(p => p.UserId == UserId);
+//            var appUser = await appDbContext.Users.FirstOrDefaultAsync(e => e.UserId == UserId);
+
+//            if (user == null || appUser == null)
+//            {
+//                return NotFound("Account not found");
+//            }
+
+//            if (user.FirstName != info.FirstName)
+//                user.FirstName = appUser.FirstName = info.FirstName;
+
+//            if (user.MiddleName != info.MiddleName)
+//                user.MiddleName = appUser.MiddleName = info.MiddleName;
+
+//            if (user.LastName != info.LastName)
+//                user.LastName = appUser.LastName = info.LastName;
+
+//            await webDbContext.SaveChangesAsync();
+//            await appDbContext.SaveChangesAsync();
+
+//            return Ok();
+//        }
+
+//        [HttpPost("profile-picture")]
+//        public async Task<IActionResult> UploadProfilePictureAsync(
+//            [FromServices] IdentityWebContext webDbContext,
+//            [FromServices] AppDbContext appDbContext,
+//            [FromServices] IBlobService blobService)
+//        {
+//            var user = await webDbContext.UserInformations.FirstOrDefaultAsync(p => p.UserId == UserId);
+//            var appUser = await appDbContext.Users.FirstOrDefaultAsync(e => e.UserId == UserId);
+
+//            if (user == null || appUser == null)
+//            {
+//                return NotFound("Account not found");
+//            }
+
+//            //  delete profile picture if existing
+//            var deleteProfilePicture = await appDbContext.FileUploads.FirstOrDefaultAsync(p => p.FileUploadId == user.ImageId);
+
+//            if (deleteProfilePicture != null)
+//            {
+//                appDbContext.Remove(deleteProfilePicture);
+//            }
+
+//            var files = Request.Form.Files;
+
+//            if (files.Count > 0)
+//            {
+//                var file = files.First();
+
+//                var bytes = new byte[file.Length];
+
+//                using (var stream = file.OpenReadStream())
+//                {
+//                    stream.Read(bytes);
+//                }
+
+//                //blob
+//                var blobUrl = $"https://cayleads.blob.core.windows.net/lead-management/{TenantId}/{file.FileName}";
+//                await blobService.UploadFormFileAsync(TenantId, file);
+
+//                var fileUploadId = GuidStr();
+
+//                var fileUpload = new FileUpload
+//                {
+//                    FileUploadId = fileUploadId,
+//                    FileName = file.FileName,
+//                    ContentDisposition = file.ContentDisposition,
+//                    ContentType = file.ContentType,
+//                    //Content = bytes,
+//                    Length = file.Length,
+//                    DateCreated = DateTime.UtcNow,
+//                    Url = blobUrl// $"api/files/{TenantId}/{fileUploadId}",
+
+//                };
+
+//                //  update profile picture of this account
+//                user.ImageId = fileUploadId;
+//                appUser.ImageId = fileUploadId;
+
+//                //  save file upload
+//                await appDbContext.AddAsync(fileUpload);
+
+//                await webDbContext.SaveChangesAsync();
+//                await appDbContext.SaveChangesAsync();
+//            }
+
+//            return Ok();
+//        }
+
+//        [HttpPost("updatePhoneNumber")]
+//        public async Task<IActionResult> UpdatePhoneNumber(
+//            [FromServices] IdentityWebContext webDbContext,
+//            [FromServices] AppDbContext appDbContext,
+//            [FromBody] UpdatePhoneNumberInfo info)
+//        {
+//            var user = await webDbContext.Users.FirstOrDefaultAsync(p => p.Id == UserId);
+//            var appUser = await appDbContext.Users.FirstOrDefaultAsync(e => e.UserId == UserId);
+
+//            if (user == null || appUser == null)
+//            {
+//                return NotFound("Account not found");
+//            }
+
+//            if (user.PhoneNumber == info.PhoneNumber)
+//            {
+//                return BadRequest("New and existing phone number are the same.");
+//            }
+
+//            user.PhoneNumber = info.PhoneNumber;
+//            user.PhoneNumberConfirmed = false;
+
+//            appUser.PhoneNumber = info.PhoneNumber;
+
+//            await webDbContext.SaveChangesAsync();
+//            await appDbContext.SaveChangesAsync();
+
+//            return Ok();
+//        }
+
+//        public async Task<IActionResult> Get(
+//            [FromServices] IdentityWebContext webDbContext,
+//            [FromServices] AppDbContext appDbContext)
+//        {
+//            var sqlUser = from u in webDbContext.Users.AsNoTracking().Include(e => e.UserInformation)
+//                          where u.Id == UserId
+//                          select new
+//                          {
+//                              u.Email,
+//                              u.PhoneNumber,
+
+//                              u.UserInformation.FirstName,
+//                              u.UserInformation.MiddleName,
+//                              u.UserInformation.LastName,
+//                              u.UserInformation.Theme,
+
+//                              ProfilePictureId = u.UserInformation.ImageId
+//                          };
+
+//            var dtoUser = await sqlUser.FirstOrDefaultAsync();
+
+//            return Ok(dtoUser);
+//        }
+
+
+//        public class UpdateUserInfo
+//        {
+//            public string UserId { get; set; }
+//            public string FirstName { get; set; }
+//            public string MiddleName { get; set; }
+//            public string LastName { get; set; }
+//        }
+
+//        public class UpdatePhoneNumberInfo
+//        {
+//            public string AccountId { get; set; }
+//            public string PhoneNumber { get; set; }
+//        }
+//    }
+
+
+//}
---
+++ src/Cayent.Core/Web/Server/Controllers/StoresController.cs
@@ -0,0 +1,293 @@
+﻿//using App.CQRS.Navbar.Common.Queries.Query;
+//using Cayent.Core.Common;
+//using Cayent.Core.CQRS.Queries;
+//using Cayent.Core.Data.Fileuploads;
+//using Data.App.DbContext;
+//using Data.Identity.DbContext;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
+//using Newtonsoft.Json;
+//using System;
+//using System.Linq;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Cayent.Core.Common.Extensions;
+//using Data.App.Models.Stores;
+//using Data.App.Models.Items;
+//using System.Collections.Generic;
+//using Data.App.Models.Products;
+//using Web.Models.Stores;
+//using Data.App.Models.Stocks;
+
+//namespace Web.Controllers
+//{
+//    [Authorize]
+//    [ApiController]
+//    [Route("api/[controller]")]
+//    [Produces("application/json")]
+//    public class StoresController : BaseController
+//    {
+//        readonly IQueryHandlerDispatcher _queryHandlerDispatcher;
+
+//        public StoresController(IQueryHandlerDispatcher queryHandlerDispatcher)
+//        {
+//            _queryHandlerDispatcher = queryHandlerDispatcher ?? throw new ArgumentNullException(nameof(queryHandlerDispatcher));
+//        }
+
+//        [HttpGet("search")]
+//        public async Task<IActionResult> GetBranchStores(string c, int p, int s, string sf, int so, [FromServices] AppDbContext dbContext, CancellationToken cancellationToken = default)
+//        {
+//            var sql = from bs in dbContext.Stores
+//                      .Include(e => e.StoreProducts)
+//                        .ThenInclude(e => e.Product)
+//                      .AsNoTracking()
+
+//                      select new
+//                      {
+//                          bs.StoreId,
+//                          bs.Name,
+//                          bs.Address,
+//                          bs.Description,
+//                          Products = bs.StoreProducts.Count()//.Select(e => e.Product.Item.Name)
+//                      };
+
+//            var dto = await sql.ToPagedItemsAsync(p, s, cancellationToken);
+
+//            return Ok(dto);
+//        }
+
+//        [HttpGet("{id}")]
+//        public async Task<IActionResult> GetStores(string id, [FromServices] AppDbContext dbContext, CancellationToken cancellationToken = default)
+//        {
+//            //var sqlStoreStocks = from ss in dbContext.StoreStocks
+//            //                     .Include(e => e.Stock)
+//            //                        .ThenInclude(e => e.Item)
+//            //                     .AsNoTracking()
+
+//            //                     where ss.StoreId == id
+
+//            //                     select new StoreInfo.StoreStock
+//            //                     {
+//            //                         StockId = ss.StockId,
+//            //                         ItemId = ss.Stock.ItemId,
+//            //                         Serial = ss.Stock.Serial,
+//            //                         Quantity = ss.Quantity,
+//            //                         SafetyQuantity = ss.SafetyQuantity,
+//            //                         ReorderQuantity = ss.ReorderQuantity,
+//            //                     };
+
+//            //var storeStocks = await sqlStoreStocks.ToListAsync();
+
+//            var sqlStoreProducts = from sp in dbContext.StoreProducts
+//                                   .Include(e => (e).Product)
+//                                        .ThenInclude(e => (e as Product).ProductImages)
+//                                   .Include(e => e.Product)
+//                                        .ThenInclude(e => (e as Product).ProductPrices)
+//                                   .AsNoTracking()
+
+//                                   where sp.StoreId == id
+
+//                                   select new StoreInfo.StoreProduct
+//                                   {
+//                                       ProductId = sp.Product.ProductId,
+//                                       Name = sp.Product.Item.Name,
+//                                       MultiPack = sp.Product.MultiPack,
+//                                       PrimaryImageUrl = sp.Product.PrimaryImageUrl,
+//                                       Price = sp.Product.ProductPrices.First().Price
+//                                   };
+
+//            var storeProducts = await sqlStoreProducts.ToListAsync();
+
+
+
+
+
+//            var sql = from bs in dbContext.Stores.AsNoTracking()
+//                          //.Include(e => e.StoreProducts)
+//                          //    .ThenInclude(e => (e as StoreProduct).Product)
+//                          //        .ThenInclude(e => (e as Product).ProductImages)
+//                          //.Include(e => e.StoreProducts)
+//                          //    .ThenInclude(e => (e as StoreProduct).Product)
+//                          //        .ThenInclude(e => (e as Product).ProductPrices)
+//                          //.Include(e => e.StoreProducts)
+//                          //    .ThenInclude(e => (e as StoreProduct).Product)
+//                          //        .ThenInclude(e => e.Item)
+
+//                      where bs.StoreId == id
+
+//                      select new StoreInfo
+//                      {
+//                          StoreId = bs.StoreId,
+//                          Name = bs.Name,
+//                          Description = bs.Description,
+//                          Address = bs.Address,
+//                          GeoX = bs.GeoX,
+//                          GeoY = bs.GeoY,
+//                          Token = bs.ConcurrencyToken
+//                      };
+
+//            var dto = await sql.FirstOrDefaultAsync(cancellationToken);
+
+//            //storeProducts.ForEach(sp =>
+//            //{
+//            //    sp.Stocks = storeStocks.Where(e => e.ItemId == sp.ProductId).ToList();
+//            //});
+
+//            dto.Products = storeProducts;
+
+
+//            return Ok(dto);
+//        }
+
+//        [HttpPost]
+//        public async Task<IActionResult> AddStoreAsync([FromBody] AddStoreInfo info, [FromServices] AppDbContext dbContext, CancellationToken cancellationToken = default)
+//        {
+//            var branchStore = new Store
+//            {
+//                StoreId = GuidStr(),
+//                Name = info.Name,
+//                Description = info.Description,
+//                Address = info.Address,
+//                GeoX = info.GeoX,
+//                GeoY = info.GeoY,
+//                Active = true,
+//            };
+
+//            await dbContext.AddAsync(branchStore, cancellationToken);
+
+//            await dbContext.SaveChangesAsync(cancellationToken);
+
+//            return Ok(branchStore.StoreId);
+//        }
+
+//        [HttpPut]
+//        public async Task<IActionResult> EditStoreAsync([FromBody] EditStoreInfo info, [FromServices] AppDbContext dbContext, CancellationToken cancellationToken = default)
+//        {
+//            var data = await dbContext.Stores.FirstOrDefaultAsync(e => e.StoreId == info.StoreId, cancellationToken);
+
+//            //data.ThrowIfNullOrAlreadyUpdated(info.Token, GuidStr());
+
+//            data.Name = info.Name;
+//            data.Description = info.Description;
+//            data.Address = info.Address;
+//            data.GeoX = info.GeoX;
+//            data.GeoY = info.GeoY;
+
+//            await dbContext.SaveChangesAsync(cancellationToken);
+
+//            return Ok(info.StoreId);
+//        }
+
+
+//        [HttpPost("set-branch-store/{id}")]
+//        public async Task<IActionResult> SetStore(string id, [FromServices] AppDbContext dbContext, CancellationToken cancellationToken = default)
+//        {
+//            var data = await dbContext.Stores.FirstOrDefaultAsync(e => e.StoreId == id, cancellationToken);
+
+//            if (data == null)
+//                return NotFound();
+
+//            Response.Cookies.Append("StoreId", data.StoreId, new CookieOptions { Secure = true, HttpOnly = true });
+//            Response.Cookies.Append("StoreName", data.Name, new CookieOptions { Secure = true, HttpOnly = true });
+
+//            return Ok();
+//        }
+
+
+//        [HttpGet("{id}/unassigned")]
+//        public async Task<IActionResult> GetUnassignedProducts(string id, [FromServices] AppDbContext dbContext, CancellationToken cancellationToken = default)
+//        {
+//            var sql = from p in dbContext.Products.AsNoTracking()
+
+//                      where !dbContext.StoreProducts.Any(e => e.StoreId == id && e.ProductId == p.ProductId)
+
+//                      select new
+//                      {
+//                          p.ProductId,
+//                          p.Item.Name,
+//                          p.PrimaryImageUrl
+//                      };
+//            var data = await sql.ToListAsync(cancellationToken);
+
+//            return Ok(data);
+//        }
+
+//        [HttpPut("assign-product")]
+//        public async Task<IActionResult> AssignProducts([FromBody] AssignStoreProductInfo info, [FromServices] AppDbContext dbContext, CancellationToken cancellationToken = default)
+//        {
+//            var existing = await dbContext.StoreProducts
+//                .FirstOrDefaultAsync(e => e.StoreId == info.StoreId && e.ProductId == info.ProductId)
+//                ;
+
+//            if (existing == null)
+//            {
+//                var newStoreProduct = new StoreProduct
+//                {
+//                    StoreId = info.StoreId,
+//                    ProductId = info.ProductId
+//                };
+
+//                //var stock = await dbContext.Stocks.FirstOrDefaultAsync(e => e.ItemId == info.ProductId);
+
+//                //if (stock == null)
+//                //{
+//                //    stock = new Stock
+//                //    {
+//                //        StockId = GuidStr(),
+//                //        ItemId = info.ProductId,
+//                //        Serial = "",
+//                //    };
+
+//                //    await dbContext.AddAsync(stock, cancellationToken);
+//                //}
+
+
+//                //var newStoreStock = new StoreStock
+//                //{
+//                //    StoreId = info.StoreId,
+//                //    StockId = stock.StockId,
+//                //};
+
+//                //await dbContext.AddRangeAsync(new object[] { newStoreProduct, newStoreStock }, cancellationToken);
+
+//                await dbContext.AddAsync(newStoreProduct, cancellationToken);
+
+//                await dbContext.SaveChangesAsync(cancellationToken);
+//            }
+
+//            return Ok();
+//        }
+
+//        [HttpPut("unassign-product")]
+//        public async Task<IActionResult> UnssignProduct([FromBody] UnassignStoreProductInfo info, [FromServices] AppDbContext dbContext, CancellationToken cancellationToken = default)
+//        {
+
+//            var existing = await dbContext.StoreProducts
+//                .FirstOrDefaultAsync(e => e.StoreId == info.StoreId && e.ProductId == info.ProductId)
+//                ;
+
+//            if (existing != null)
+//            {
+//                dbContext.Remove(existing);
+
+//                //var storeStocks = await dbContext.StoreStocks.Include(e => e.Stock).Where(e => e.StoreId == info.StoreId && e.Stock.ItemId == info.ProductId).ToListAsync();
+
+//                //if (storeStocks != null)
+//                //{
+//                //    dbContext.RemoveRange(storeStocks);
+//                //    var stocks = storeStocks.Select(e => e.Stock);
+
+//                //    dbContext.RemoveRange(stocks);
+//                //}
+
+//                await dbContext.SaveChangesAsync(cancellationToken);
+//            }
+
+//            return Ok();
+//        }
+//    }
+
+
+//}
