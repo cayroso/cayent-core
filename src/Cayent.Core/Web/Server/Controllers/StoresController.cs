7c359c4e67e1ba90c763d8ac04c707e7984f91c2
ChristopherAyroso <caydev2010@gmail.com>
2023-01-18 23:05:39 +0800

    test

--- src/Cayent.Core/Web/Server/Controllers/StoresController.cs
+++ src/Cayent.Core/Web/Server/Controllers/StoresController.cs
@@ -291,3 +291,68 @@
 
 
 //}
+
+
+
+//[ApiController]
+//[Route("api/[controller]")]
+//[Produces("application/json")]
+//public class FilesController : BaseController
+//{
+//    [HttpGet("{id}")]
+//    [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Any)]
+//    public async Task<IActionResult> GetFileAsStreamAsync([FromServices] AppDbContext appDbContext, string id)
+//    {
+
+//        var data = await appDbContext.FileUploads
+//            .AsNoTracking()
+//            .FirstOrDefaultAsync(p => p.FileUploadId == id);
+
+//        if (data == null)
+//            return NotFound();
+
+//        var stream = new MemoryStream(data.Content.Length);
+
+//        stream.Write(data.Content, 0, data.Content.Length);
+//        stream.Position = 0;
+
+//        return File(stream, data.ContentType, data.FileName, true);
+//    }
+
+//    [HttpGet("{tenantId}/{id}")]
+//    [ResponseCache(Duration = 600, Location = ResponseCacheLocation.Any)]
+//    public async Task<IActionResult> GetFileAsStreamAsync(
+//        [FromServices] IConfiguration configuration,
+//        [FromServices] DbContextOptions<AppDbContext> options,
+//        [FromServices] IdentityWebContext dbContext, string tenantId, string id)
+//    {
+
+//        var tenant = await dbContext
+//            .Tenants
+//            .AsNoTracking()
+//            .SingleOrDefaultAsync(p => p.TenantId == tenantId);
+
+//        if (tenant == null)
+//            return NotFound();
+
+//        var dummyProvider = new DummyTenantProvider(tenant);
+//        var appDbContext = new AppDbContext(options, configuration, dummyProvider);
+
+//        var data = await appDbContext.FileUploads
+//                    .AsNoTracking()
+//                    .FirstOrDefaultAsync(e => e.FileUploadId == id);
+
+//        if (data == null)
+//            return NotFound();
+
+//        if (data.Content == null)
+//            return NotFound();
+
+//        var stream = new MemoryStream(data.Content.Length);
+
+//        stream.Write(data.Content, 0, data.Content.Length);
+//        stream.Position = 0;
+
+//        return File(stream, data.ContentType, data.FileName, true);
+//    }
+//}
